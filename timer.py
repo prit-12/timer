55c200fdf46c8b1ff16b7241a03c519471fdc80f
prit-12 <159299510+prit-12@users.noreply.github.com>
2024-02-13 22:22:01 -0500

    Update timer.py

--- timer.py
+++ timer.py
@@ -5,19 +5,20 @@ Upon timer expiry, user will see the time up meme and sound notification.
 timer.py uses the time library to help keep track of time
 """
 
-
-# This program is timer that counts down
-
-
 import time # The time library has a sleep function that will pause the script for a specifized amount of time
+import random
 from PIL import Image # the pillow library makes it easy to display images 
 
-im = Image.open("times-up.jpeg")
+def nerve_of_steel_game():
+    # Display the initial game instruction
+    print("Players stand")
 
-# ask user to enter desired countdown time
-set_time = int(input("Please set your timer in seconds: "))
+# Wait for a random time between 5 and 25 seconds
+random_sleep_time = random.randint(5, 25)
+print(f"Random waiting time is {random_sleep_time} seconds. Prepare to sit!")
+time.sleep(random_sleep_time)
 
-time.sleep(set_time)
+print("Time Up. Last to sit down wins.")
 
+# Display the "times up" meme
 im.show()
-
